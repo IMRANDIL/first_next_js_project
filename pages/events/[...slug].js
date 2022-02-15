93a0b75547ecda46423911dac3bd18fb9925f7df
IMRANDIL <aliimranadil2@gmail.com>
2022-02-15 18:16:51 +0530

    yes started

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FilteredEventspage = () => {
+    return (
+        <div>
+            <h1>Filtered Events Page</h1>
+        </div>
+    )
+}
+
+export default FilteredEventspage
